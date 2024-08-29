9df43fb09cb445d00977b23d3efe589293ce3d4b
Mariana Cardoso <marianacardoso.tds2.senai@gmail.com>
2024-08-29 10:51:28 -0300

    feat: add some requests

--- src/server.js
+++ src/server.js
@@ -8,6 +8,53 @@ const serverPort = process.env.PORT || 3000
 const app = express()
 app.use(express.json()) // json: Java Script Object Notation
 
+const emocoes = [
+    {
+        id: 1,
+        nome: "Nostalgia",
+        cor: "Cinza"
+    },
+
+    {
+        id: 2,
+        nome: "Nojinho",
+        cor: "Verde"
+    },
+
+    {
+        id: 3,
+        nome: "Raiva",
+        cor: "Vermelho"
+    },
+]
+
+const personagens = [
+    {
+        id: 100,
+        nome: "Relampago Mcqueen",
+        studio: "Pixar",
+        copaPistao: true,
+        relaciomento: "Sally Carrera"
+    },
+
+    {
+        id: 101,
+        nome: "Tom Mate",
+        studio: "Pixar",
+        copaPistao: false,
+        relaciomento: "Holly"
+
+    },
+
+    {
+        id: 102,
+        nome: "Sally Carrera",
+        studio: "Pixar",
+        copaPistao: false,
+        relaciomento: "Relampago Mcqueen"
+    },
+]
+
 app.get("/",(req, res) => {
     return res.status(200).send({message:"Hello, World!" })
 })
@@ -15,6 +62,17 @@ app.get("/",(req, res) => {
 app.get("/2tds2",(req, res) => {
     return res.status(200).send({message:"Hello, World!" })
 })
+
+app.get("/emocoes",(req, res) => {
+    return res.status(200)
+    .send(emocoes)
+})
+
+app.get("/personagens",(req, res) => {
+    return res.status(200)
+    .send(personagens)
+})
+
 
 app.listen(serverPort, () => {
     console.log(`😍 Server started on http//localhost:${serverPort}`)
