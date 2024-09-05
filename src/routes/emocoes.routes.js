bf8b5ab335a9f8fcebb58eafad6ef4096de6508d
Mariana Cardoso <marianacardoso.tds2.senai@gmail.com>
2024-09-05 17:19:23 -0300

    feat: create get by id method

--- src/routes/emocoes.routes.js
+++ src/routes/emocoes.routes.js
@@ -20,12 +20,14 @@ let emocoes = [
         nome: "Raiva",
         cor: "Vermelho"
     },
-]
+];
 
+//Rota para buscar todas as emocões
 emocoesRoutes.get("/",(req, res) => {
     return res.status(200).send(emocoes)
-})
+});
 
+//Criar uma nova emoção
 emocoesRoutes.post("/",(req, res) => {
     const{nome, cor} = req.body
     const newEmotion = {
@@ -34,7 +36,25 @@ emocoesRoutes.post("/",(req, res) => {
         cor: cor
     }
     emocoes.push(newEmotion)
-    return res.status(201).send(emocoes)
+    return res.status(201).send(newEmotion)
+});
+
+emocoesRoutes.get("/:id", (req, res) => {
+    const{id} = req.params; 
+
+    //console.log(id);
+    const emocao = emocoes.find((emotion) => emotion.id == id)
+
+    if(!emocao){ 
+        return res.status(404).send({
+        message: "Emoção não encontrada!",
+        });
+    }
+
+    return  res.status(200).send({
+        message: "Emoção encontrada",
+        emocao
+    })
 });
 
 export default emocoesRoutes;
